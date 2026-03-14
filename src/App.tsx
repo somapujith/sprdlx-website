c046af5c8295c764b7f1788fe15cc92c4929b74f
sprdlx <Hello@sprdlx.com>
2026-03-14 23:59:46 +0530

    fix: resolve choppy scroll by removing conflicting RAF loops and optimizing Lenis config

--- src/App.tsx
+++ src/App.tsx
@@ -21,15 +21,9 @@ export default function App() {
       autoRaf: true,
       smoothWheel: true,
       duration: 1.2,
+      lerp: 0.1,
+      easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
     });
-
-    function raf(time: number) {
-      lenis.raf(time)
-      requestAnimationFrame(raf)
-    }
-
-    requestAnimationFrame(raf);
-
 
     // Cleanup
     return () => {
